29acae12ef4e34e0bd3ea2f49d4266ac1200cbea
Matthew Kerns <matthew.kerns@MKerns-Macbook-Pro.local>
2020-04-14 19:02:48 -0400

    Updates

---
+++ precache-manifest.60981ee09907791d11acbf6ed24a5c2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36a4fb17edebfa5853e61241fa02c80",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b55f46258db7ec4e49fd",
+    "url": "/monsters-rolodex/static/css/main.d396b5cd.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b55f46258db7ec4e49fd",
+    "url": "/monsters-rolodex/static/js/main.8e8abd8c.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
